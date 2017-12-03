16f1c55b11d24100ff3283122aed8c27626cc3ba
Nolan Handali <nolanhandali@gmail.com>
2017-12-02 18:57:55 -0800

    added mulligan strategy, and wrote a feature extractor

--- fireplace-master/tests/full_game.py
+++ fireplace-master/tests/full_game.py
@@ -3,20 +3,32 @@ import sys; sys.path.append("..")
 from fireplace import cards
 from fireplace.exceptions import GameOver
 from fireplace.utils import play_full_game
-
+import collections
 
 def test_full_game(numgames = 1):
 	try:
+		weights = collections.defaultdict(int)
 		count = 0
 		total = 0
 		for _ in range(numgames):
 			game = play_full_game()
 			if game.loser != game.players[0] :
 				count += 1
+				for card in game.startCards:
+					weights[card.id] +=1
+				for card in game.oppCards:
+					weights[card.id] -=1
+			else:
+				for card in game.startCards:
+					weights[card.id] -=1
+				for card in game.oppCards:
+					weights[card.id] +=1
 			total += 1
 		print("Winrate: ", count/float(total))
+		print("Card Weights", weights)
 	except GameOver:
 		print("Game completed normally.")
+
 
 
 def main():
